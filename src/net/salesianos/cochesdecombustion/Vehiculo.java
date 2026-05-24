215351e16fd7b9958fbfc621d7e79b1bbd97c552
J44v11 <javiergarcialopez101@gmail.com>
2026-05-24 15:25:02 +0100

    feat: Agregar clase abstracta Vehiculo con atributos básicos

---
+++ src/net/salesianos/cochesdecombustion/Vehiculo.java
@@ -0,0 +1,10 @@
+package net.salesianos.cochesdecombustion;
+
+public abstract class Vehiculo {
+
+    protected String color;
+    protected int numeroPuertas;
+    protected int numeroRuedas;
+    protected String modelo;
+    protected String matricula;
+}
