27f1be7fc2bc6e73362de8b116c11d19ccb974f5
J44v11 <javiergarcialopez101@gmail.com>
2026-05-24 15:32:23 +0100

    feat: Añado clase CocheCombustion que extiende de Vehiculo con atributos de si el motor esta encendido y del combustible

---
+++ src/net/salesianos/cochesdecombustion/CocheCombustion.java
@@ -0,0 +1,7 @@
+package net.salesianos.cochesdecombustion;
+
+public class CocheCombustion extends Vehiculo {
+
+    protected boolean motorCombustionEncendido;
+    protected int litrosCombustible;
+}
