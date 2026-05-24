4170351aedc07fd6803abe0e22acf492d98ad683
J44v11 <javiergarcialopez101@gmail.com>
2026-05-24 15:33:02 +0100

    feat: Añado constructor a la clase CocheCombustion para inicializar atributos de motor y combustible

--- src/net/salesianos/cochesdecombustion/CocheCombustion.java
+++ src/net/salesianos/cochesdecombustion/CocheCombustion.java
@@ -4,4 +4,12 @@ public class CocheCombustion extends Vehiculo {
 
     protected boolean motorCombustionEncendido;
     protected int litrosCombustible;
+
+    public CocheCombustion(String color, int numeroPuertas, int numeroRuedas, String modelo, String matricula,
+            boolean motorCombustionEncendido, int litrosCombustible) {
+        super(color, numeroPuertas, numeroRuedas, modelo, matricula);
+        this.motorCombustionEncendido = motorCombustionEncendido;
+        this.litrosCombustible = litrosCombustible;
+    }
+
 }
