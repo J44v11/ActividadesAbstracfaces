e3f0707c344f8d4042a24b396c8347ccc35d0dbd
J44v11 <javiergarcialopez101@gmail.com>
2026-05-24 15:25:17 +0100

    feat: Añadir constructor a la clase abstracta Vehiculo

--- src/net/salesianos/cochesdecombustion/Vehiculo.java
+++ src/net/salesianos/cochesdecombustion/Vehiculo.java
@@ -7,4 +7,12 @@ public abstract class Vehiculo {
     protected int numeroRuedas;
     protected String modelo;
     protected String matricula;
+
+    public Vehiculo(String color, int numeroPuertas, int numeroRuedas, String modelo, String matricula) {
+        this.color = color;
+        this.numeroPuertas = numeroPuertas;
+        this.numeroRuedas = numeroRuedas;
+        this.modelo = modelo;
+        this.matricula = matricula;
+    }
 }
