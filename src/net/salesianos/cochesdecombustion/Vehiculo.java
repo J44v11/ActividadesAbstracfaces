66e69cfc278a81886a1c88f4bc1f3c474b18017a
J44v11 <javiergarcialopez101@gmail.com>
2026-05-24 15:26:07 +0100

    feat: Agrego métodos getter y setter a la clase abstracta Vehiculo

--- src/net/salesianos/cochesdecombustion/Vehiculo.java
+++ src/net/salesianos/cochesdecombustion/Vehiculo.java
@@ -15,4 +15,45 @@ public abstract class Vehiculo {
         this.modelo = modelo;
         this.matricula = matricula;
     }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+
+    public int getNumeroRuedas() {
+        return numeroRuedas;
+    }
+
+    public void setNumeroRuedas(int numeroRuedas) {
+        this.numeroRuedas = numeroRuedas;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
 }
